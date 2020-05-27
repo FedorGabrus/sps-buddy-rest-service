f98f6c1cb45c9c4cb9eeecb07828ee5c3e343d8f
Fedor Gabrus <hello@fedorgabrus.id.au>
2020-05-27 22:20:09 +0930

    Initial setup

---
+++ src/main/java/au/edu/tafesa/spsbuddyrestservice/SpsBuddyRestServiceApplication.java
@@ -0,0 +1,13 @@
+package au.edu.tafesa.spsbuddyrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpsBuddyRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpsBuddyRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/edu/tafesa/spsbuddyrestservice/SpsBuddyRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.edu.tafesa.spsbuddyrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpsBuddyRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
